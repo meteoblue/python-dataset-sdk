05f337c0f54d400dad2e5d4b57fa6fc600706677
Jonas Wolff <jonas.wolff@meteoblue.com>
2020-08-19 01:23:55 +0200

    tested parallel querying

--- mbdataset/client.py
+++ mbdataset/client.py
@@ -29,10 +29,11 @@ class Client(object):
 
     def __init__(self, apikey: str):
         self._config = ClientConfig(apikey)
-        logging.basicConfig(filename=self._config.log_file, level=logging.INFO)
-
-        if not os.path.exists(self._config.tmp_directory):
-            os.makedirs(self._config.tmp_directory)
+        logging.basicConfig(
+            filename=self._config.log_file,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            level=logging.DEBUG,
+            datefmt='%Y-%m-%d %H:%M:%S')
 
         self._loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self._loop)
@@ -40,11 +41,11 @@ class Client(object):
     def __del__(self):
         self._loop.close()
 
-    async def __http_get(self, url: str, retry_count=0):
+    async def __http_get(self, session, url: str, retry_count=0):
         logging.debug('Getting url %s' % url)
         logging.debug('Retry count: %s' % retry_count)
 
-        async with self._session.get(url) as response:
+        async with session.get(url) as response:
             json = await response.json()
 
             # return if successful
@@ -59,12 +60,12 @@ class Client(object):
                     # 408: HTTP request timeout
                     # 500-599: HTTP backend error
                     await asyncio.sleep(1)
-                    return self.__http_get(url, retry_count + 1)
+                    return self.__http_get(session, url, retry_count + 1)
 
             logging.error('API returned error: %s' % response.content)
             raise Exception("API returned error", response.content)
 
-    async def _job_finished(self, queue_id: int):
+    async def _job_finished(self, session, queue_id: int):
         """
         Wait until job has been processed by backend servers
         :param queue_id: id of queued job
@@ -72,14 +73,14 @@ class Client(object):
         """
         url = self._config.status_url % str(queue_id)
         while True:
-            json = await self.__http_get(url)
+            json = await self.__http_get(session, url)
             logging.debug('Job status is %s' % json['status'])
             if json['status'] == 'finished':
                 break
             logging.info('Waiting 5 more seconds for job to complete.')
             await asyncio.sleep(5)
 
-    async def _submit_query(self, params: dict, retry_count=0):
+    async def _submit_query(self, session, params: dict, retry_count=0):
         """
         Try to submit query to api
         :param params: query parameters for meteoblue dataset api
@@ -88,7 +89,7 @@ class Client(object):
         url = self._config.query_url % self._config.apikey
         logging.debug('Posting data to url %s' % url)
         logging.debug('Retry count: %s' % retry_count)
-        async with self._session.post(url, json=params) as response:
+        async with session.post(url, json=params) as response:
             json = await response.json()
 
             if 200 <= response.status <= 299:
@@ -102,7 +103,7 @@ class Client(object):
                     # 408: HTTP request timeout
                     # 500-599: HTTP backend error
                     await asyncio.sleep(1)
-                    return self._submit_query(params, retry_count + 1)
+                    return self._submit_query(session, params, retry_count + 1)
 
                 # check if request needs to be executed on job queue
                 if 400 <= response.status <= 499:
@@ -110,12 +111,12 @@ class Client(object):
                         if json['error_message'] == 'This job must be executed on a job-queue':
                             logging.info("Queueing job")
                             params["runOnJobQueue"] = True
-                            return await self._submit_query(params)
+                            return await self._submit_query(session, params)
 
             logging.error('API returned error: %s' % response.content)
             raise Exception("API returned error", response.content)
 
-    async def _fetch_result(self, queue_id):
+    async def _fetch_result(self, session, queue_id):
         """
         Fetch api results and write to temp file
         :param queue_id: id of queued job
@@ -124,7 +125,7 @@ class Client(object):
         # self.__http_get() could be used here, too if we solve caching differently
         url = self._config.result_url % queue_id
         logging.debug('Fetching result(s) from url %s' % url)
-        async with self._session.get(url) as response:
+        async with session.get(url) as response:
             return await response.json()
 
     async def _query(self, params: dict):
@@ -133,17 +134,19 @@ class Client(object):
         :param params: query parameters for meteoblue dataset api
         :return: nothing/void
         """
-        async with aiohttp.ClientSession() as self._session:
-            queue = await self._submit_query(params)
+        async with aiohttp.ClientSession() as session:
+            queue = await self._submit_query(session, params)
             logging.info("Waiting until job has finished")
-            await self._job_finished(queue['id'])
-            return await self._fetch_result(queue['id'])
+            await self._job_finished(session, queue['id'])
+            return await self._fetch_result(session, queue['id'])
 
-    def query(self, params: dict):
+    async def query(self, params: dict):
         """
         Query async dataset api interface
         :param params: query parameters for meteoblue dataset api
         :return: json result data
         """
 
-        return self._loop.run_until_complete(self._query(params))
+        await self._query(params)
+        return await self._query(params)
+        #return self._loop.run_until_complete(self._query(params))
