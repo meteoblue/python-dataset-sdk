9bae9c6292e1bf06eb48211ee928793cabc951fb
thiebaut.fischer <thiebaut.fischer@meteoblue.com>
2021-02-18 18:36:29 +0100

    change get_valid_caches

--- meteoblue_dataset_sdk/caching/filecache.py
+++ meteoblue_dataset_sdk/caching/filecache.py
@@ -31,7 +31,7 @@ class FileCache(Cache):
         query_dir_path = os.path.join(self.cache_path, hashed_params)
         if not os.path.exists(query_dir_path):
             await aios.mkdir(query_dir_path)
-        if self._get_valid_caches(query_dir_path):
+        if self._get_latest_cached_file(query_dir_path):
             return
         temp_file_path = os.path.join(query_dir_path, f"{file_name}~")
         async with aiofiles.open(temp_file_path, "wb") as file:
@@ -45,16 +45,15 @@ class FileCache(Cache):
         query_dir_path = os.path.join(self.cache_path, query_hash)
         if not os.path.exists(query_dir_path):
             return
-        valid_caches = self._get_valid_caches(query_dir_path)
-        if not valid_caches:
+        most_recent_file = self._get_latest_cached_file(query_dir_path)
+        if not most_recent_file:
             return
         try:
-            most_recent_file = valid_caches[0]
             most_recent_file_path = os.path.join(query_dir_path, most_recent_file)
             async with aiofiles.open(most_recent_file_path, "rb") as f:
                 return await f.read()
         except FileNotFoundError:
-            print(os.path.join(self.cache_path, valid_caches[0]), "not found")
+            print(os.path.join(self.cache_path, most_recent_file), "not found")
             return
 
     def _is_cached_query_valid(self, timestamp: str):
@@ -62,12 +61,12 @@ class FileCache(Cache):
         cache_duration = datetime.datetime.now() - key_as_datetime
         return cache_duration.seconds < self.cache_ttl
 
-    def _get_valid_caches(self, dir_path: str):
-        return sorted(
-            [
-                file
-                for file in os.listdir(dir_path)
-                if not file.find("~") and self._is_cached_query_valid(file)
-            ],
-            reverse=True,
-        )
+    def _get_latest_cached_file(self, dir_path: str):
+        cached_files = [
+            file
+            for file in os.listdir(dir_path)
+            if not file.find("~") and self._is_cached_query_valid(file)
+        ]
+        if not cached_files:
+            return
+        return max(cached_files)
