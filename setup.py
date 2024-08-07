8dcce66315055512b0ee02b81b520b1b7098b6d1
Nicoletta Addimando <nicoletta.addimando@meteoblue.com>
2024-08-07 10:53:21 +0200

    Revert "try out pypitest"
    This reverts commit 4ce92ab6b23365b4f04379377c03696f672636e6.

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 setup(
     use_scm_version=True,
-    long_description=long_description,
-    long_description_content_type='text/markdown'
 )
